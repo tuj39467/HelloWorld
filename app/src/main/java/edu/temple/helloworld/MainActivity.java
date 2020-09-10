4491f396df71931a7de759d565f65b13f05904fd
Christine Gregotski <tuj39467@temple.edu>
2020-09-09 20:00:29 -0400

    HelloWorld edited

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,6 +3,9 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String message = "You have clicked the button.";
+                Toast.makeText(MainActivity.this, message,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
